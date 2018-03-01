77dda97efa7cffbd95cfbcc20a86fae1fb877fc1
PaulC2017 <paulc2017@att.net>
2018-03-01 10:53:06 -0500

    Added Search jobs by term bopth specific and all columns, added display styling and list all jobs in the data base

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,37 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        [HttpPost][HttpGet]
+        [Route("/Search/Results")]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+          List<Dictionary<string,string>> jobs = new List<Dictionary<string, string>>();
+
+            if (searchType.Equals("all") && searchTerm != null)
+            {
+                jobs = JobData.FindByValue(searchTerm);
+            }
+            else if (searchTerm == null)
+            {
+                jobs = JobData.FindAll();
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+          ViewBag.columns = ListController.columnChoices;
+          ViewBag.jobs = jobs;
+          return View("Index");
+        }
+
     }
+
+    
+
+      
+
+
+
+
+
 }
